7fe7c0cfcf491cc8b89468e0c0c82b6d9070e918
Gliston <daggergad7@gmail.com>
2025-11-04 19:58:44 +0000

    add rcu_utils

---
+++ src/mako/masstree/rcu_utils.hh
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <cstddef>
+
+#ifndef NDEBUG
+#include <mutex>
+#include <unordered_set>
+#endif
+
+#include "../macros.h"
+#include "../rcu.h"
+
+namespace masstree::detail {
+
+#ifndef NDEBUG
+inline std::mutex& handle_mutex() {
+    static std::mutex mutex;
+    return mutex;
+}
+
+inline std::unordered_set<const void*>& handle_allocations() {
+    static std::unordered_set<const void*> allocations;
+    return allocations;
+}
+
+inline void track_alloc(const void* ptr) {
+    if (!ptr) {
+        return;
+    }
+    std::lock_guard<std::mutex> lock(handle_mutex());
+    handle_allocations().insert(ptr);
+}
+
+inline void track_release(const void* ptr) {
+    if (!ptr) {
+        return;
+    }
+    std::lock_guard<std::mutex> lock(handle_mutex());
+    handle_allocations().erase(ptr);
+}
+
+inline bool is_tracked(const void* ptr) {
+    if (!ptr) {
+        return true;
+    }
+    std::lock_guard<std::mutex> lock(handle_mutex());
+    return handle_allocations().find(ptr) != handle_allocations().end();
+}
+#else
+inline void track_alloc(const void*) {}
+inline void track_release(const void*) {}
+inline bool is_tracked(const void*) {
+    return true;
+}
+#endif
+
+inline void assert_tracked(const void* ptr) {
+#ifndef NDEBUG
+    if (ptr && !is_tracked(ptr)) {
+        INVARIANT(false && "Masstree payload pointer was not allocated via masstree::rcu_allocate");
+    }
+#else
+    (void)ptr;
+#endif
+}
+
+} // namespace masstree::detail
+
+namespace masstree {
+
+// @unsafe
+inline void* rcu_allocate(size_t bytes) {
+    void* ptr = rcu::s_instance.alloc(bytes);
+    detail::track_alloc(ptr);
+    return ptr;
+}
+
+// @unsafe
+inline void* rcu_allocate_static(size_t bytes) {
+    void* ptr = rcu::s_instance.alloc_static(bytes);
+    detail::track_alloc(ptr);
+    return ptr;
+}
+
+// @unsafe
+inline void rcu_deallocate(void* ptr, size_t bytes) {
+    detail::track_release(ptr);
+    rcu::s_instance.dealloc(ptr, bytes);
+}
+
+// @unsafe
+inline void rcu_deallocate_rcu(void* ptr, size_t bytes) {
+    detail::track_release(ptr);
+    rcu::s_instance.dealloc_rcu(ptr, bytes);
+}
+
+// @unsafe
+inline void rcu_free_with(void* ptr, rcu::deleter_t fn) {
+    detail::track_release(ptr);
+    rcu::s_instance.free_with_fn(ptr, fn);
+}
+
+inline void debug_assert_tracked(const void* ptr) {
+    detail::assert_tracked(ptr);
+}
+
+} // namespace masstree
